b2bdc7d43d749d3c9a813ea00c57580336ca7fc3
avraamdever <maestro@gmail.com>
2023-03-07 21:54:47 +0300

    Add Randomize

--- Example004_Delenie/Program.cs
+++ Example004_Delenie/Program.cs
@@ -1,5 +1,7 @@
-﻿double numberA = 3;
-double numberB = 5;
+﻿double numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 double result=numberA/numberB;
 Console.WriteLine(result);
 Console.WriteLine ("Мы молодцы");
