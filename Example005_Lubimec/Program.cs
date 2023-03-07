83aacb4d4904339c4e3230f78ffee747c83118c9
avraamdever <maestro@gmail.com>
2023-03-07 22:05:15 +0300

    Add Example005_Lubimec

---
+++ Example005_Lubimec/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "анна")
+{
+  Console.Write ("Добрый день госпожа Анна");
+}
+else
+{
+  Console.Write ($"Привет, {username}");
+}
